a50ff374db6e8c9ce94c5a089807f813cb76bb6d
Toan Le <thanhtoanhcm1602@gmail.com>
2025-07-17 05:23:34 +0700

    - Config Eslint and tailwind. - Add Shadcn.

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,3 +1,5 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import Image from "next/image";
 
 export default function Home() {
@@ -26,6 +28,8 @@ export default function Home() {
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
+          <Button className="p-10" variant={"outline"}>Test 1</Button>
+          <Input placeholder="Type something..." />
           <a
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
             href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
