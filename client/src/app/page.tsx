aac91742d83f485f45f77f62d59b46a4e8e93cb9
Toan Le <thanhtoanhcm1602@gmail.com>
2025-07-18 04:46:39 +0700

    - Add image in nextjs.

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,66 +1,27 @@
-import Card from "@/app/Card";
-import CardCopy from "@/app/Card/CardCopy";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    // font-sans 
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        {/* <h1 className="text-4xl roboto-thin">Hello</h1> */}
-        <h1 className="text-4xl font-thin">Hello</h1>
-        <h1 className="text-4xl font-normal">Hello world</h1>
-        <h1 className="text-4xl font-sans font-normal">Hello there</h1>
-        <Card />
-        <CardCopy />
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+    <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+      <div className="w-[700px] h-[700px] bg-red-300">
+        <Image
+          src="/images/garrett_parker.jpg"
+          alt="photo"
+          width={500}
+          height={500}
+          // quality={50}
+          // className="w-[500px] h-[500px]"
+        />
+
+        <Image
+          src="https://images.unsplash.com/photo-1505832018823-50331d70d237?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8NGslMjBsYW5kc2NhcGV8ZW58MHx8MHx8fDA%3D"
+          alt="photo"
+          width={200}
+          height={200}
+          quality={50}
+          // className="w-[500px] h-[500px]"
+        />
+      </div>
+    </main>
   );
 }
